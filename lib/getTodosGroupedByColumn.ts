dbc235ba1fc6a1c37bbf1a15b0ad5e7da84db759
gflacruz <glacruz@zoho.com>
2023-05-27 17:35:45 -0400

    added boards and columns

--- lib/getTodosGroupedByColumn.ts
+++ lib/getTodosGroupedByColumn.ts
@@ -12,5 +12,40 @@ export const getTodosGroupedByColumn = async () => {
                 todos: []
             })
         }
+
+        acc.get(todo.status)!.todos.push({
+            $id: todo.$id,
+            $createdAt: todo.$createdAt,
+            title: todo.title,
+            status: todo.status,
+            ...(todo.image && {image: JSON.parse(todo.image)})
+        })
+
+        return acc
     }, new Map<TypedColumn, Column>())
+
+
+    const columnTypes: TypedColumn[] = ['todo', 'inprogress', 'done']
+    for (const columnType of columnTypes) {
+        if (!columns.get(columnType)) {
+            columns.set(columnType, {
+                id: columnType,
+                todos: []
+            })
+        }
+    }
+
+    console.log(columns)
+
+    const sortedColumns = new Map(
+        Array.from(columns.entries()).sort(
+        (a,b) => columnTypes.indexOf(a[0]) - columnTypes.indexOf(b[0])
+        )
+    )
+
+    const board: Board = {
+        columns: sortedColumns
+    }
+
+    return board
 }
